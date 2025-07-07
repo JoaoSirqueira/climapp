c1b982e1cdd2b12df3a14096bacecec35c0fcda3
DevSirq <joaosirqueirasantos@gmail.com>
2025-07-07 17:59:02 -0300

    Iniciando a primeira tela do projeto

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,25 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Image, Text, TouchableOpacity } from "react-native";
+import { LinearGradient } from 'expo-linear-gradient'
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+    <LinearGradient
+      colors={["#00457D", "#05051F"]}
+      style={styles.container}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+      <Image source={require("../assets/images/Logo.png")}/>
+      <Image source={require("../assets/images/Weather.png")}></Image>
+      <Text>Boas-vindas!</Text>
+
+      <TouchableOpacity>
+        <Text>Entrar</Text>
+      </TouchableOpacity>
+    </LinearGradient>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+})
