a737873404be8950fe50e91ba3ea091f2df578a6
DevSirq <joaosirqueirasantos@gmail.com>
2025-07-15 10:48:55 -0300

    Adicionando Fetch para obter dados da API

--- app/[cityName].tsx
+++ app/[cityName].tsx
@@ -1,8 +1,25 @@
 import { View } from "react-native"
 import { useLocalSearchParams } from 'expo-router'
+import { useEffect } from "react"
 
 const CityDetails = () => {
     const searchParams = useLocalSearchParams()
+
+    const handleData = async () => {
+        try {
+            const response = await fetch('https://climapp-api.vercel.app/api')
+            const responseJSON = await response.json()
+
+            console.log(responseJSON)
+        } catch (e) {
+            console.log(e)
+        }
+    }
+
+    useEffect(() => {
+        handleData()
+    }, [])
+
     return (
         <View>
 
